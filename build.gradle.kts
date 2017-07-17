fb6fc60fac112e4e7989630d9338e60a013688ad
TheMrMilchmann <themrmilchmann@gmail.com>
2017-07-17 23:20:17 +0200

    Update Gradle distro

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,6 @@ allprojects {
 
 tasks {
     "wrapper"(Wrapper::class) {
-        distributionUrl = "https://repo.gradle.org/gradle/dist-snapshots/gradle-kotlin-dsl-4.1-20170707154639+0000-all.zip"
+        distributionUrl = "https://repo.gradle.org/gradle/dist-snapshots/gradle-kotlin-dsl-4.1-20170717152239+0000-all.zip"
     }
 }
